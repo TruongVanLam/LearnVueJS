0d71d56449864bf8eda719c28bfe5286ca5eaa28
TruongVanLam <truongvanlam176@gmail.com>
2022-09-06 17:34:18 +0700

    props

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,18 @@ import Todo from './components/todo.vue'
 import Counter from './components/counter.vue'
 import EventHandling from './components/event_handling.vue'
 import Binding from './components/binding.vue'
+import Props from './components/props.vue'
 export default {
-  components: { Todo, Counter, EventHandling, Binding }
+  components: { Todo, Counter, EventHandling, Binding, Props },
+  data() {
+    return {
+        post: {
+            id: 1,
+            title: 'My life is best than'
+        },
+        favorited: true,
+    }
+  },
 }
 </script>
 
@@ -14,4 +24,7 @@ export default {
   <event-handling></event-handling>
   <br/>
   <binding></binding>
+  <props v-bind:post="post"></props>
+  <button @click="post.title = 'update'">Change title cha</button>
+  <p> Thang cha: {{post.title}}</p>
 </template>
---
+++ src/components/props.vue
@@ -0,0 +1,24 @@
+<script>
+    export default {
+        data() {
+            return {
+                content: ""
+            }
+        },
+        props: {
+            post: {
+                type: Object,
+            }
+        },
+        methods: {
+            handleChangeTitle() {
+                return this.post.title = "Update complete!"
+            }
+        }
+    }
+</script>
+
+<template>
+    <h2> Thang con: {{post.title}}</h2>
+    <button @click="handleChangeTitle()">Change title con</button>
+</template>
